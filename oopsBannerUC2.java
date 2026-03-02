95afd9040c7449160e1e08c9166875fa56a9bcc9
Gauri <gauritaneja04@gmail.com>
2026-03-02 13:04:06 +0530

    uc3

--- oopsBannerUC2.java
+++ oopsBannerUC2.java
@@ -1,1 +1,21 @@
-public class OppsBannerUC2 {
+public class OopsBanner {
+    public static void main(String[] args) {
+        // Constructing each row manually using string concatenation
+        String line1 = "  *** " + "  *** " + " ***** " + " **** ";
+        String line2 = " * * " + " * * " + " * * " + " * * ";
+        String line3 = " * * " + " * * " + " * * " + " * * ";
+        String line4 = " * * " + " * * " + " ***** " + " *** ";
+        String line5 = " * * " + " * * " + " * " + " * ";
+        String line6 = " * * " + " * * " + " * " + " * ";
+        String line7 = "  *** " + "  *** " + " * " + " **** ";
+
+        // Sequential print statements for visual pattern
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
